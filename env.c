f308094dcdc0c33cea8682ccc5f0db50861c0395
RahmaShaban <RahmaShaban@gmail.com>
2023-07-27 18:42:45 -0700

    a

--- env.c
+++ env.c
@@ -71,18 +71,17 @@ int myu(info_t *in)
 }
 
 /**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * popu - the popu
+ * @in: Structure
  * Return: Always 0
  */
-int populate_env_list(info_t *info)
+int popu(info_t *in)
 {
-	list_t *node = NULL;
-	size_t i;
+	list_t *n = NULL;
+	size_t ia;
 
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
+	for (ia = 0; environ[ia]; ia++)
+		add_n(&n, environ[ia], 0);
+	in->env = n;
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -193,11 +193,11 @@ void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
 /* toem_environ.c */
-char *_getenv(info_t *, const char *);
-int _myenv(info_t *);
-int _mysetenv(info_t *);
-int _myunsetenv(info_t *);
-int populate_env_list(info_t *);
+int myen(info_t *in);
+char *gete(info_t *in, const char *na);
+int mys(info_t *in);
+int myu(info_t *in);
+int popu(info_t *in);
 
 /* toem_getenv.c */
 char **get_environ(info_t *);
