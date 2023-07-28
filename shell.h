40ad1b666609d0f3ba4c5242022bae9fa708dee1
RahmaShaban <RahmaShaban@gmail.com>
2023-07-28 16:42:36 -0700

    a

--- shell.h
+++ shell.h
@@ -112,53 +112,49 @@ typedef struct builtin
 
 
 /* toem_shloop.c */
-int hsh(info_t *, char **);
-int find_builtin(info_t *);
-void find_cmd(info_t *);
-void fork_cmd(info_t *);
+int hsh(info_t *in, char **v);
+int f_buil(info_t *in);
+void fi_cm(info_t *in);
+void for_cm(info_t *in);
 
-/* toem_parser.c */
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
+/* toem_par.c */
+int is_cm(info_t *in, char *p);
+char *dup_ch(char *p, int st, int sp);
+char *find_p(info_t *in, char *p, char *cm);
 
 /* loophsh.c */
 int loophsh(char **);
 
-/* toem_errors.c */
-void _eputs(char *);
-int _eputchar(char);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
+/* toem_er.c */
+void _ep(char *s);
+int _epc(char ca);
+int _puf(char ca, int f);
+int _pus(char *s, int f);
 
-/* toem_string.c */
-int _strlen(char *);
-int _strcmp(char *, char *);
-char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
+/* toem_str.c */
+int _str(char *sa);
+int strc(char *s1a, char *s2a);
+char *s_w(const char *hay, const char *ne);
+char *_strc(char *de, char *sr);
 
 /* toem_string1.c */
-char *_strcpy(char *, char *);
-char *_strdup(const char *);
-void _puts(char *);
-int _putchar(char);
+char *_st(char *de, char *sr);
+char *_strdu(const char *st);
+void _pu(char *st);
+int _puc(char ca);
 
-/* toem_exits.c */
-char *_strncpy(char *, char *, int);
-char *_strncat(char *, char *, int);
-char *_strchr(char *, char);
+/* toem_ex.c */
+char *_st(char *d, char *sa, int na);
+char *_strc(char *d, char *s, int na);
+char *strch(char *sa, char ca);
 
-/* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
-
-/* toem_realloc.c */
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
-void *_realloc(void *, unsigned int, unsigned int);
+/* toem_rec.c */
+char *_mem(char *sa, char ba, unsigned int na);
+void ffree(char **p);
+void *_re(void *pt, unsigned int o_size, unsigned int n_size);
 
 /* toem_memory.c */
-int bfree(void **);
+int bfree(void **p);
 
 /* to_at.c */
 int inter(info_t *in);
@@ -166,31 +162,35 @@ int is_de(char ca, char *de);
 int isalp(int ca);
 int _at(char *sa);
 
-/* toem_errors1.c */
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
+/* toem_er1.c */
+int _err(char *sa);
+void p_err(info_t *in, char *es);
+int p_d(int in, int f);
+char *con_n(long int nu, int b, int f);
+void re_co(char *b);
 
 /* to_bu.c */
 int mye(info_t *in);
 int myc(info_t *in);
 int myh(info_t *in);
 
-/* toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
+/* toem_bu2.c */
+int myhis(info_t *in);
+int un_al(info_t *in, char *s);
+int s_al(info_t *in, char *s);
+int p_al(list_t *n);
 
-/*toem_getline.c */
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
+/*toem_gl.c */
+ssize_t i_bu(info_t *in, char **b, size_t *l);
+ssize_t g_in(info_t *in);
+ssize_t re_b(info_t *in, char *b, size_t *ia);
+int _getl(info_t *in, char **p, size_t *l);
+void sig(__attribute__((unused))int s_n);
 
 /* toem_getinfo.c */
-void clear_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
+void cl_in(info_t *in);
+void se_in(info_t *in, char **a);
+void fr_in(info_t *in, int al);
 
 /* toem_environ.c */
 int myen(info_t *in);
@@ -199,37 +199,37 @@ int mys(info_t *in);
 int myu(info_t *in);
 int popu(info_t *in);
 
-/* toem_getenv.c */
-char **get_environ(info_t *);
-int _unsetenv(info_t *, char *);
-int _setenv(info_t *, char *, char *);
+/* toem_gev.c */
+char **g_env(info_t *in);
+int _uns(info_t *in, char *v);
+int _set(info_t *in, char *v, char *val);
 
-/* toem_history.c */
-char *get_history_file(info_t *info);
-int write_history(info_t *info);
-int read_history(info_t *info);
-int build_history_list(info_t *info, char *buf, int linecount);
-int renumber_history(info_t *info);
+/* toem_his.c */
+char *get_his_f(info_t *in);
+int wr_his(info_t *in);
+int re_his(info_t *in);
+int b_his_l(info_t *in, char *b, int lin);
+int ren_his(info_t *in);
 
 /* toem_lists.c */
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-void free_list(list_t **);
+list_t *ad_no(list_t **h, const char *s, int n);
+list_t *add_n(list_t **h, const char *s, int n);
+size_t p_list_s(const list_t *ha);
+int delete_node_at_index(list_t **h, unsigned int in);
+void fr_li(list_t **h_ptr);
 
-/* toem_lists1.c */
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
+/* toem_lis1.c */
+size_t lis_len(const list_t *ha);
+char **l_to_s(list_t *h);
+size_t pr_l(const list_t *ha);
+list_t *node_starts_with(list_t *n, char *pre, char ca);
+ssize_t get_node_index(list_t *h, list_t *n);
 
 /* toem_vars.c */
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
-int replace_string(char **, char *);
+int is_ch(info_t *in, char *bu, size_t *Pa);
+void c_cha(info_t *in, char *bu, size_t *pa, size_t ia, size_t len);
+int rep_al(info_t *in);
+int rep_v(info_t *in);
+int rep_str(char **o, char *n);
 
 #endif
--- va.c
+++ va.c
@@ -1,16 +1,16 @@
 #include "shell.h"
 
 /**
- * is_chain - test if current char in buffer is a chain delim
- * @in: the parameter struct
- * @bu: the char 
- * @p: address of current position in bu
+ * is_ch - test if current
+ * @in: the par
+ * @bu: the char
+ * @Pa: addres
  *
- * Return:  return the delim
+ * Return: retur
  */
-int is_chain(info_t *in, char *bu, size_t *P)
+int is_ch(info_t *in, char *bu, size_t *Pa)
 {
-	size_t j = *P;
+	size_t j = *Pa;
 
 	if (bu[j] == '|' && bu[j + 1] == '|')
 	{
@@ -24,36 +24,36 @@ int is_chain(info_t *in, char *bu, size_t *P)
 		j++;
 		in->cmd_buf_type = CMD_AND;
 	}
-	else if (bu[j] == ';') /* found end of this command */
+	else if (bu[j] == ';')
 	{
-		bu[j] = 0; /* replace semicolon with null */
+		bu[j] = 0;
 		in->cmd_buf_type = CMD_CHAIN;
 	}
 	else
 		return (0);
-	*p = j;
+	*pa = j;
 	return (1);
 }
 
 /**
- * check_chain - checks we should continue chaining based on last status
- * @in: the parameter struct
- * @bu: the char buf
- * @p: address of current position in bu
- * @i: starting position in bu
+ * c_cha - checks we should
+ * @in: the paramet
+ * @bu: the buf
+ * @pa: addre
+ * @ia: starting
  * @len: len of bu
  *
  * Return: Void
  */
-void check_chain(info_t *in, char *bu, size_t *p, size_t i, size_t len)
+void c_cha(info_t *in, char *bu, size_t *pa, size_t ia, size_t len)
 {
-	size_t j = *p;
+	size_t j = *pa;
 
 	if (in->cmd_buf_type == CMD_AND)
 	{
 		if (in->status)
 		{
-			bu[i] = 0;
+			bu[ia] = 0;
 			j = len;
 		}
 	}
@@ -61,94 +61,94 @@ void check_chain(info_t *in, char *bu, size_t *p, size_t i, size_t len)
 	{
 		if (!in->status)
 		{
-			bu[i] = 0;
+			bu[ia] = 0;
 			j = len;
 		}
 	}
 
-	*p = j;
+	*pa = j;
 }
 
 /**
- * replace_alias - replaces an aliases in the tokenized string
- * @in: the parameter struct
+ * rep_al - replace
+ * @in: the paramet
  *
  * Return: 1 if replaced
  */
-int replace_alias(info_t *in)
+int rep_al(info_t *in)
 {
 	int i;
-	list_t *node;
-	char *p;
+	list_t *n;
+	char *pa;
 
 	for (i = 0; i < 10; i++)
 	{
-		node = node_starts_with(info->alias, info->argv[0], '=');
-		if (!node)
+		n = node_starts_with(in->alias, in->argv[0], '=');
+		if (!n)
 			return (0);
 		free(in->argv[0]);
-		p = _strchr(node->str, '=');
-		if (!p)
+		pa = strch(n->str, '=');
+		if (!pa)
 			return (0);
-		p = _strdup(p + 1);
-		if (!p)
+		pa = _strdu(pa + 1);
+		if (!pa)
 			return (0);
-		in->argv[0] = p;
+		in->argv[0] = pa;
 	}
 	return (1);
 }
 
 /**
- * replace_vars - replaces vars in the tokenized string
- * @in: the parameter struct
+ * rep_v - replace
+ * @in: the para
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 if repla
  */
-int replace_vars(info_t *in)
+int rep_v(info_t *in)
 {
 	int i = 0;
-	list_t *node;
+	list_t *n;
 
 	for (i = 0; in->argv[i]; i++)
 	{
 		if (in->argv[i][0] != '$' || !in->argv[i][1])
 			continue;
 
-		if (!_strcmp(in->argv[i], "$?"))
+		if (!strc(in->argv[i], "$?"))
 		{
-			replace_string(&(in->argv[i]),
-				_strdup(convert_number(info->status, 10, 0)));
+			rep_str(&(in->argv[i]),
+				_strdu(con_n(in->status, 10, 0)));
 			continue;
 		}
-		if (!_strcmp(in->argv[i], "$$"))
+		if (!strc(in->argv[i], "$$"))
 		{
-			replace_string(&(in->argv[i]),
-				_strdup(convert_number(getpid(), 10, 0)));
+			rep_str(&(in->argv[i]),
+				_strdu(con_n(getpid(), 10, 0)));
 			continue;
 		}
-		node = node_starts_with(info->env, &in->argv[i][1], '=');
-		if (node)
+		n = node_starts_with(in->env, &in->argv[i][1], '=');
+		if (n)
 		{
-			replace_string(&(in->argv[i]),
-				_strdup(_strchr(node->str, '=') + 1));
+			rep_str(&(in->argv[i]),
+				_strdu(_strc(n->str, '=') + 1));
 			continue;
 		}
-		replace_string(&in->argv[i], _strdup(""));
+		rep_str(&in->argv[i], _strdu(""));
 
 	}
 	return (0);
 }
 
 /**
- * replace_string - replaces string
- * @old: address of old string
- * @new: new string
+ * rep_str - repla
+ * @o: add
+ * @n: new
  *
- * Return: 1 if replaced, 0 otherwise
+ * Return: 1 if
  */
-int replace_string(char **old, char *new)
+int rep_str(char **o, char *n)
 {
-	free(*old);
-	*old = new;
+	free(*o);
+	*o = n;
 	return (1);
 }
